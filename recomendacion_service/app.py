4258609bd704a5a10b903cf403120656128202e6
ofmunozm <of.munoz@uniandes.edu.co>
2025-02-21 07:58:33 -0500

    ajuste kafka y funcionamiento

--- recomendacion_service/app.py
+++ recomendacion_service/app.py
@@ -1,31 +1,39 @@
 from flask import Flask, request, jsonify
-from kafka import KafkaProducer
+from confluent_kafka import Producer
 import json
+import time
 
 app = Flask(__name__)
 
 # Configurar el productor de Kafka
-producer = KafkaProducer(
-    bootstrap_servers="kafka:9092",
-    value_serializer=lambda v: json.dumps(v).encode("utf-8")
-)
+p = Producer({'bootstrap.servers': 'localhost:9092'})
+print('Kafka Producer has been initiated...')
+
+@app.route('/')
+def home():
+    return "Servicio de Recomendaciones en ejecución"
 
 @app.route('/recomendar', methods=['POST'])
 def recomendar():
     data = request.json
     video_id = data.get("video_id")
-    
+
     if not video_id:
         return jsonify({"error": "Falta el video_id"}), 400
 
     # Generar recomendaciones
     recomendaciones = [f"video_recomendado_{i}" for i in range(1, 4)]
-    
-    # Enviar a Kafka
-    mensaje = {"video_id": video_id, "recomendaciones": recomendaciones}
-    producer.send("recomendaciones", mensaje)
 
-    return jsonify(mensaje)
+    # Crear el mensaje para Kafka (convertir a JSON)
+    m = {"video_id": video_id, "recomendaciones": recomendaciones}
+    message = json.dumps(m).encode('utf-8')  # Convertir el diccionario a JSON y luego a bytes
+
+    # Enviar el mensaje a Kafka
+    p.produce('user-tracker', message)
+    p.flush()  # Asegura que se envíen todos los mensajes
+    print(f"Mensaje enviado a Kafka: {message}")
+
+    return jsonify(m)  # Retorna el mensaje enviado como respuesta
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002, debug=True)
--- ventas_service/app.py
+++ ventas_service/app.py
@@ -1,10 +1,35 @@
+from confluent_kafka import Consumer
 from flask import Flask
+import threading
 
 app = Flask(__name__)
 
+# Configurar el consumidor de Kafka
+c = Consumer({'bootstrap.servers': 'localhost:9092', 'group.id': 'python-consumer', 'auto.offset.reset': 'earliest'})
+print('Kafka Consumer has been initiated...')
+
 @app.route("/")
 def home():
-    return "Servicio de Ventas en ejecución"
+    return "Servicio de Recomendaciones en ejecución"
+
+def consume_messages():
+    c.subscribe(['user-tracker'])
+
+    while True:
+        msg = c.poll(1.0)  # Timeout de 1 segundo
+        if msg is None:
+            continue
+        if msg.error():
+            print('Error: {}'.format(msg.error()))
+            continue
+        data = msg.value().decode('utf-8')
+        print(f"Mensaje recibido: {data}")
+    c.close()
 
 if __name__ == "__main__":
+    # Iniciar el consumidor en un hilo separado
+    consumer_thread = threading.Thread(target=consume_messages)
+    consumer_thread.start()
+
+    # Iniciar la aplicación Flask
     app.run(host="0.0.0.0", port=5001, debug=True)
