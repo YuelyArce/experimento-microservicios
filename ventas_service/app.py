e807f499c9445ebcaa05afdff11818f23c70ddd5
ofmunozm <of.munoz@uniandes.edu.co>
2025-02-22 23:34:46 -0500

    ajuste de app producer

--- ventas_service/app.py
+++ ventas_service/app.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, jsonify
-from confluent_kafka import Producer
 from flask_restful import Api
 from vistas import Home, Ventas
-
-
 
 app = Flask(__name__)
 app_context = app.app_context()
@@ -12,10 +9,6 @@ app_context.push()
 api = Api(app)
 api.add_resource(Home, '/')
 api.add_resource(Ventas, '/registroVenta')
-
-# Configurar el productor de Kafka
-p = Producer({'bootstrap.servers': 'localhost:9092'})
-print('Kafka Producer has been initiated...')
 
 
 if __name__ == '__main__':
