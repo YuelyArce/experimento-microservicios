c56451aab91ee758269ac7616d9397c150048698
ofmunozm <of.munoz@uniandes.edu.co>
2025-03-15 23:27:08 -0500

    se agrega verificacion de lista negra en modificacion de producto

--- logistica_service/vistas/vistas.py
+++ logistica_service/vistas/vistas.py
@@ -3,8 +3,10 @@ from flask import request, jsonify, make_response
 from flask_restful import Resource
 from utils.security import validate_token
 from modelos.modelos import db, Producto
+from os import getenv
 
 AUDITORIA_URL = "http://localhost:5004/evento"
+BLACK_URL =  "http://127.0.0.1:3001/black"
 
 class validar_token(Resource):
     def post(self):
@@ -28,16 +30,34 @@ class validar_token(Resource):
 
 class ModificarProducto(Resource):
     def put(self, producto_id):
-        # Validar token
         headers = dict(request.headers)
+
+        # Validar que el token está presente
         if "Token" not in headers:
             return make_response(jsonify({"message": "Invalid Token"}), 401)
 
+        # Validar token
         data_token, status_code = validate_token(headers["Token"])
         if status_code != 200:
             return make_response(jsonify(data_token), status_code)
+
+        # Extraer usuario del token validado
         usuario = data_token.get("usuario", "Desconocido")
         rol = data_token.get("role")
+
+        # Verificar si el usuario está en la lista negra
+        header = {'TOKEN': getenv('TOKEN_BLACK')}
+        try:
+            response = requests.get(BLACK_URL, json={'usuario': usuario}, headers=header)
+            response.raise_for_status()
+            usuario_in_black = response.json().get('black', False)
+        except requests.RequestException as e:
+            return make_response(jsonify({"message": "Error consultando lista negra", "error": str(e)}), 500)
+
+        # Si el usuario está en la lista negra, bloquearlo
+        if usuario_in_black:
+            return make_response(jsonify({"message": "Usuario bloqueado, no tiene permisos"}), 403)
+
 
         # Buscar el producto en la base de datos
         producto = Producto.query.get(producto_id)
